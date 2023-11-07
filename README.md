e770a2007b138ab50b4b75beae7f760b2403b4d6
tiger <44923556+eeeeaa@users.noreply.github.com>
2023-11-07 20:08:48 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamic interface demo
+
+demo of some dynamic interfaces following Odin project
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  resolve: {
+    extensions: ['*', '.js']
+  }
+};
